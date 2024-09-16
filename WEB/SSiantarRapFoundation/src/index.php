431a105bd4bc8203ad13ba5c3a5cb60752ccf77e
Martua Raja Doli  Pangaribuan <135083526+Rajdoll@users.noreply.github.com>
2024-09-16 08:46:10 +0700

    Update index.php

--- WEB/SSiantarRapFoundation/src/index.php
+++ WEB/SSiantarRapFoundation/src/index.php
@@ -37,7 +37,7 @@ if (isset($_GET['host']) && is_string($_GET['host']) && preg_match('/^(https?:\/
         curl_close($ch);
     }
 } else {
-    $output = "Give a URL first.";
+    $output = "URL Dulu Bos!";
 }
 
 ?>
@@ -46,7 +46,7 @@ if (isset($_GET['host']) && is_string($_GET['host']) && preg_match('/^(https?:\/
 <html>
 
 <head>
-    <title>Daa Crawler</title>
+    <title>seseref crawler</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/styles/default.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/highlight.min.js"></script>
     <script>
@@ -60,24 +60,24 @@ if (isset($_GET['host']) && is_string($_GET['host']) && preg_match('/^(https?:\/
         <div class="py-8 px-4 mx-auto max-w-screen-xxl sm:py-16 lg:px-6">
             <div class="mx-auto max-w-screen-sm text-center">
                 <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">
-                    Sit Down, We Will Crawl it!
+                    Hundul Ho asa huperiksa linkmu!
                 </h2>
                 <form action="">
                     <div class="mb-6 mt-4">
                         <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Website URL
+                            Linkmu
                         </label>
                         <input type="text" id="default-input" name="host" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800 mt-5">
                             <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                                Visit
+                                Lao
                             </span>
                         </button>
                     </div>
                 </form>
                 <div class="dark:bg-gray-800 dark:border-gray-700 rounded-lg shadow-md border border-gray-200">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-                        Output
+                        Bereng
                     </h5>
                     <hr class="my-3 h-px border-0 dark:bg-gray-700">
                     <div class="rounded-lg overflow-hidden">
